ff690d58f42bc72e690b4a8958d0fdafc4f96957
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-03-24 08:31:31 +0700

    Composable Function - Use simple code to handle Text element

--- app/src/main/java/io/github/fatimazza/jetpackcompose/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/jetpackcompose/MainActivity.kt
@@ -3,28 +3,15 @@ package io.github.fatimazza.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.fatimazza.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android")
         }
     }
 }
@@ -37,7 +24,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }
