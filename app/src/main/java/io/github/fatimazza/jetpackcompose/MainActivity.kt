2f55f7843944daf07cc7a801b3a7a9f28d9969c8
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-03-24 13:13:18 +0700

    Image - Add image inside Column

--- app/src/main/java/io/github/fatimazza/jetpackcompose/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/jetpackcompose/MainActivity.kt
@@ -3,9 +3,11 @@ package io.github.fatimazza.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,11 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun GreetingCard(msg: Message) {
-    Column{
+    Column {
+        Image(
+            painterResource(R.drawable.sample_image),
+            "Contact profile picture",
+        )
         Text(text = msg.author)
         Text(text = msg.body)
     }
