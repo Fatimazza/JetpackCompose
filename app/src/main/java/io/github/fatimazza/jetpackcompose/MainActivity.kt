e44c1be13c919cc08f84d22b0885441f9681783d
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-03-24 15:03:53 +0700

    Color - Add color in Text and Image border

--- app/src/main/java/io/github/fatimazza/jetpackcompose/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/jetpackcompose/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -42,13 +44,17 @@ fun GreetingCard(msg: Message) {
                 // set Image size and shape
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant
+            )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
